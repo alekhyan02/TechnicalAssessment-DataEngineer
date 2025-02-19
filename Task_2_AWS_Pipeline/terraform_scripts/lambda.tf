ef971e71f01b0c47fd2f5508ff918298f7adfd79
alekhyan02 <alekhya.n02@gmail.com>
2025-02-18 23:29:42 -0500

    Add files via upload

--- Task_2_AWS_Pipeline/terraform_scripts/lambda.tf
+++ Task_2_AWS_Pipeline/terraform_scripts/lambda.tf
@@ -1,13 +1,28 @@
 resource "aws_lambda_function" "marketing_data_etl" {
-  function_name = "marketing_data_etl"
-  role          = "arn:aws:iam::977099003709:role/lambda_s3_glue_role"  # Update with correct IAM role
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  
-  filename      = "lambda_function.zip"  # Ensure this file exists in your Terraform directory
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  filename         = "lambda_function.zip"
+  function_name    = "marketing_data_etl"
+  role            = aws_iam_role.lambda_role.arn
+  handler         = "lambda_function.lambda_handler"
+  runtime         = "python3.8"
+  timeout         = 30
+  memory_size     = 128
+  depends_on      = [aws_iam_role.lambda_role]
+}
 
-  timeout       = 30
-  memory_size   = 128
+resource "aws_lambda_permission" "allow_s3_trigger" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.marketing_data_etl.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.marketing_data.arn
+}
+
+resource "aws_s3_bucket_notification" "s3_trigger" {
+  bucket = aws_s3_bucket.marketing_data.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.marketing_data_etl.arn
+    events             = ["s3:ObjectCreated:*"]
+  }
 }
 
